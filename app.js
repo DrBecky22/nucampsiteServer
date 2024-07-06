379c41e8754d121ffcaf74ef1ee16dd017d681f2
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-07-05 23:46:43 -0400

    commit
    passport and token exercises

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-
+const config = require('./config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
-
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/nucampsite';
+const passport = require('passport');
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {});
+
 
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
@@ -31,36 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('lottalongstringofcharacters')); //provide a secret key to sign the cookie
 
-app.use(session({
-  name: 'session-id',
-  secret: 'lottalongstringofcharacters',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()   //stores the session information in the server's file system - hard drive
-}));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      } else {
-    if (req.session.user === 'authenticated') {    
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
-  } 
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
